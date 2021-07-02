9bde8c8d22451b6d9c6c706962491e52b19bc91d
iamsurajdev <surajbiswas367@gmail.com>
2021-07-03 00:31:22 +0530

    configure and style the buttons

--- lib/widget/button.dart
+++ lib/widget/button.dart
@@ -1,10 +1,29 @@
 import 'package:flutter/material.dart';
 
 class Button extends StatelessWidget {
-  const Button({Key? key}) : super(key: key);
+  final backgroundColor;
+  final textColor;
+  final String buttonText;
+
+  // const Button({Key? key, this.backgroundColor,this.buttonText, this.textColor}) : super(key: key);
+  Button({this.backgroundColor, this.buttonText = "", this.textColor});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: EdgeInsets.all(10),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(10),
+        child: Container(
+          color: backgroundColor,
+          child: Center(
+            child: Text(
+              buttonText,
+              style: TextStyle(color: textColor, fontSize: 24),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
